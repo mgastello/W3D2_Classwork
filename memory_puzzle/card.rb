96bb8a0b8807cb6b08a394e1a287218ad3d09fd7
Dan Holodak <danholodak@gmail.com>
2023-01-24 12:16:27 -0500

    started card class

---
+++ memory_puzzle/card.rb
@@ -0,0 +1,27 @@
+class Card
+
+    def initialize(face, value)
+        @face = face
+        @value = value
+    end
+
+    def face
+        @face
+    end
+
+    def value
+        @value
+    end
+
+    def hide
+        @face = false
+    end
+
+    def reveal
+
+    end
+
+    def == (card)
+
+    end
+end
